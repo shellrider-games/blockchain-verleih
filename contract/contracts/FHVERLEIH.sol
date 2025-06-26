48f0efa08023a1161c40a2d95641d3923fe215f5
Georg Becker <georg.becker@shellrider.com>
2025-06-26 12:08:43 +0200

    Adds whitelist to contract

--- contract/contracts/FHVERLEIH.sol
+++ contract/contracts/FHVERLEIH.sol
@@ -8,6 +8,7 @@ contract VERLEIH is ERC721 {
     uint256 private _nextTokenId;
     
     mapping(uint256 => string) serialNumberOf;
+    mapping(address => bool) _isApprovedRecipient;
 
     modifier onlyContractOwner() {
         require(msg.sender == _contractOwner, "Only the contract owner can call this function.");
@@ -17,6 +18,21 @@ contract VERLEIH is ERC721 {
     constructor() ERC721("UASDevice", "UASD"){
         _contractOwner = msg.sender;
         _nextTokenId = 1;
+        _isApprovedRecipient[_contractOwner] = true;
+    }
+
+    function amIContractOwner() external view returns (bool) {
+        return msg.sender == _contractOwner;
+    }
+
+    function addApprovedRecipient(address recipient) external onlyContractOwner {
+        require(recipient != address(0), "Invalid address!");
+        _isApprovedRecipient[recipient] = true;
+    }
+
+    function removeApprovedRecipient(address recipient) external onlyContractOwner {
+        require(recipient != address(0), "Invalid address!");
+        _isApprovedRecipient[recipient] = false;
     }
 
     function createNewDevice(string memory _serialNumber) external onlyContractOwner returns (uint256) {
@@ -28,11 +44,24 @@ contract VERLEIH is ERC721 {
     }
 
     function transferContractOwnership(address newOwner) external onlyContractOwner {
-        require(newOwner != address(0), "Invalid address.");
+        require(newOwner != address(0), "Invalid address!");
         _contractOwner = newOwner;
+        _isApprovedRecipient[newOwner] = true;
     }
 
     function serialNumber(uint _id) external view returns (string memory){
         return serialNumberOf[_id];
+    }
+
+    function isApprovedRecipient(address recipient) public view returns (bool) {
+        return _isApprovedRecipient[recipient];
+    }
+
+    function _update(address to, uint256 tokenId, address auth) internal virtual override returns (address) {
+        address from = super._update(to, tokenId, auth);
+        if(to != address(0)){
+            require(_isApprovedRecipient[to], "Recipient is not on the approved list.");
+        }
+        return from;
     }
 }
